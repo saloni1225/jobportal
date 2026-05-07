47443addb6a3c076e4c617f3b11b9bacfcf10872
Saloni Kumari <saloni01april@gmail.com>
2026-05-07 15:20:40 +0530

    updated backend

--- Register.jsx
+++
@@ -1,9 +0,0 @@
-// Replace lines 186-199 to always show the register button
-
-<Button
-  type="submit"
-  disabled={loading}
-  className={`register-button ${loading ? 'loading' : ''}`}
->
-  {loading ? 'Registering...' : 'Register'}
-</Button>
